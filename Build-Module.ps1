8a987ef3091a0fd3710abc21150d5b7af1bc41dc
Brian Lalonde <brianary@gmail.com>
2019-11-02 22:33:54 -0700

    Fix rebuilding help

--- Build-Module.ps1
+++ Build-Module.ps1
@@ -2,5 +2,5 @@ pushd $PSScriptRoot
 dotnet build
 dotnet publish
 $framework = (Select-Xml '/Project/PropertyGroup/TargetFramework/text()' .\src\SelectXmlExtensions\SelectXmlExtensions.fsproj).Node.Value
-New-ExternalHelp docs -OutputPath src\SelectXmlExtensions\bin\Debug\$framework\publish
+New-ExternalHelp docs -OutputPath src\SelectXmlExtensions\bin\Debug\$framework\publish -Force
 popd
