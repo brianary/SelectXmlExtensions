a9967141d09e61772236445f215b1f9f5d61e4d4
Brian Lalonde <brianary@gmail.com>
2019-11-04 21:22:09 -0800

    Add build enhancements; remove nonfunctional version number

--- Build-Module.ps1
+++ Build-Module.ps1
@@ -1,6 +1,20 @@
+#Requires -Version 3
+[CmdletBinding()] Param(
+[Parameter(Position=0)][ValidateSet('Debug','Release')][string] $Configuration = 'Debug',
+[switch] $Clean
+)
+
 pushd $PSScriptRoot
-dotnet build --version-suffix $(Get-Date -f yyyyMMddHHmmss)
-dotnet publish
+if($Clean)
+{
+	Remove-Item .\src\SelectXmlExtensions\bin\Debug\$framework\publish -Recurse -Force -ErrorAction SilentlyContinue
+	Remove-Item .\src\SelectXmlExtensions\bin\Release\$framework\publish -Recurse -Force -ErrorAction SilentlyContinue
+	dotnet clean
+}
+dotnet build --configuration $Configuration
+dotnet publish --configuration $Configuration
 $framework = (Select-Xml '/Project/PropertyGroup/TargetFramework/text()' .\src\SelectXmlExtensions\SelectXmlExtensions.fsproj).Node.Value
-New-ExternalHelp docs -OutputPath src\SelectXmlExtensions\bin\Debug\$framework\publish -Force
+if(!(Get-Command New-ExternalHelp -ErrorAction SilentlyContinue)) { Write-Warning 'Unable to update MAML help, is platyPS installed?' }
+else { New-ExternalHelp docs -OutputPath src\SelectXmlExtensions\bin\$Configuration\$framework\publish -Force }
+(Get-Item .\src\SelectXmlExtensions\bin\$Configuration\$framework\publish\SelectXmlExtensions.dll).VersionInfo
 popd
